3efdd6b5492b90ef37d06223126a08bf926ac9ac
ecossio <ecossio@outlook.com>
2022-09-09 19:12:09 -0600

    wishlist

--- client/src/app/website/pages/account/wishlist/wishlist.component.ts
+++ client/src/app/website/pages/account/wishlist/wishlist.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { WishlistService } from 'src/app/services/wishlist.service';
 
 @Component({
   selector: 'app-wishlist',
   templateUrl: './wishlist.component.html',
-  styleUrls: ['./wishlist.component.scss']
+  styleUrls: ['./wishlist.component.scss'],
 })
 export class WishlistComponent implements OnInit {
-
-  constructor() { }
+  wishes: Product[] = [];
+  constructor(private wishlistSrv: WishlistService) {}
 
   ngOnInit(): void {
+    this.wishlistSrv.getWishes().subscribe((resp) => {
+      this.wishes = resp.data;
+      console.log(resp.data);
+    });
   }
 
+  remove(id: number) {
+    this.wishlistSrv.deleteWish(id).subscribe((resp) => {
+      const idx = this.wishes.findIndex((e) => e.id == id);
+      console.log(idx)
+      if (idx >= 0) {
+        this.wishes.splice(idx, 1);
+      }
+    });
+  }
 }
